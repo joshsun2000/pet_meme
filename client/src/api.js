801e1a66ab59d87bb04704cf4cc7fbbf0b48d5ed
Joshua Sun <joshsun@berkeley.edu>
2025-04-29 01:59:52 -0700

    fix api address

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000'
 });
 
 export default API;
