8028c2bae9d269793faa4c683f1cf765d0308752
Joshua Sun <joshsun@berkeley.edu>
2025-04-29 01:28:30 -0700

    fix pathtoregexp issue again

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/api/proxy', proxyRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
     
-    app.get('/:catchAll(*)', (req, res) => {
+    app.get('/*splat', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
